fc9b188087b00fed8fecf53a3a9bd431c9586271
Singh Amitesh <amitesh.singh@corvallis.it>
2022-07-11 10:58:42 +0200

    init commit

---
+++ src/index.js
@@ -0,0 +1,7 @@
+import mycurrentLocation, { msg, name, getGreeting } from './myModule'
+import myadd, { subtract } from './math'
+
+// console.log(msg, name, mycurrentLocation, getGreeting('Singh'))
+
+console.log('result - ', myadd(5, 4))
+console.log('result - ', subtract(5, 4))
