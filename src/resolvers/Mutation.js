21cd51183c0eeea42d818ab62771687300d5751d
Singh Amitesh <amitesh.singh@corvallis.it>
2022-07-15 21:01:32 +0200

    update users

--- src/resolvers/Mutation.js
+++ src/resolvers/Mutation.js
@@ -16,6 +16,33 @@ const Mutation = {
 		db.users.push(user)
 		return user
 	},
+	updateUser(parent, args, {db}, info) {
+		console.log(args)
+		const { id, data } = args
+		const user = db.users.find( (user) => user.id === id)
+
+		if (!user) {
+			throw new Error("User not found...!!!")
+		}
+
+		if (typeof data.email === 'string') {
+			const emailTaken = db.users.some( (user) => user.email === data.email)
+
+			if (emailTaken) {
+				throw new Error("Email already use...!!!")
+			}
+			user.email = data.email
+		}
+
+		if(typeof data.name === 'string') {
+			user.name = data.name
+		}
+
+		if (typeof data.age !== 'undefined') {
+			user.age = data.age
+		}
+		return user
+	},
 	deleteUser(parent, args, {db}, info) {
 		console.log(args)
 
